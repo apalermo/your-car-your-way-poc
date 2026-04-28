e64fa3a288cc71175079d8fe7bcdcc19cd4d166b
apalermo <anthony.palermo@laposte.net>
2026-04-28 15:40:47 +0200

    refactor(database): remove useless comments for production standard

--- backend/src/main/java/com/yourcaryourway/poc/repositories/ChatMessageRepository.java
+++ backend/src/main/java/com/yourcaryourway/poc/repositories/ChatMessageRepository.java
@@ -4,11 +4,6 @@ import com.yourcaryourway.poc.entities.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Interface d'accès aux données pour l'entité ChatMessage.
- * En étendant JpaRepository, Spring génère automatiquement toutes les requêtes SQL de base
- * (save, findAll, findById, delete) sans que nous ayons à écrire de code supplémentaire.
- */
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 }
